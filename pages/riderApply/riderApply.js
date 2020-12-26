62afda5641c89ab01f81c055b0010a34f7b01126
xnx60 <1527478139@qq.com>
2020-12-26 17:00:18 +0800

    init 12.26

---
+++ pages/riderApply/riderApply.js
@@ -0,0 +1,117 @@
+import {
+  totast,
+  BASE_URL,
+  API_URL_updatePhoto,
+  TOKEN,
+  loading,
+  hideLoading,
+  STATUS_CODE_updatePhoto_SUCCESSE
+}from '../../service/config'
+import {
+  submitProve
+}from '../../service/infoSum'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgList: {
+      idCardR:{
+        name:'身份证正面',
+        label:'idCardR',
+        url:''
+      },
+      idCardeB:{
+        name:'身份证反面',
+        label:'idCardeB',
+        url:''
+      },
+      campusCard:{
+        name:'校园卡',
+        label:'campusCard',
+        url:''
+      },
+      stuIdCard:{
+        name:'学生卡',
+        label:'stuIdCard',
+        url:''
+      }
+    }, 
+    returnUrlList:{
+      idCardR:'',
+      idCardeB:'',
+      campusCard:'',
+      stuIdCard:''
+    },
+    index: null,
+    picker: ['是', '否'],
+  },
+  ChooseImage(e) {
+    const {type}=e.currentTarget.dataset 
+    const imgUrl=`imgList.${type}.url` 
+    const _this=this   
+    wx.chooseImage({
+      count: 1, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], //从相册选择
+      success: (res) => {
+        const localUrl= res.tempFilePaths[0]             
+          this.setData({
+            [imgUrl]: localUrl
+          })
+          wx.uploadFile({
+            url:  BASE_URL+API_URL_updatePhoto, 
+            filePath: localUrl,
+            name: 'file',
+            header:{
+              'content-type':'multipart/form-data',
+            },
+            formData: {
+              name: type
+            },      
+            success (res){
+              
+              loading('正在上传')
+              // console.log(res);
+              const data = JSON.parse(res.data)　
+              console.log(data);              
+              if(data.code==STATUS_CODE_updatePhoto_SUCCESSE){
+                totast(data.msg)
+                const returnUrlItem=data.data.url
+                const returnNameItem=data.data.name
+                let returnUrl=_this.data.returnUrl
+                for (let key in returnUrl){
+                  if(key==returnNameItem){
+                    returnUrl[key]=returnUrlItem
+                  }
+                }
+                console.log(returnUrl);
+                
+              }  else {
+                totast(data.msg)
+              }       
+            },
+            fail (reject){
+              totast('系统出错，请重试')      
+            },
+            // complete (){
+            //   console.log(type);             
+            // }
+          })
+      }
+    });
+  },
+  
+  _submitProve(returnUrlList){
+    const driverId=wx.getStorageSync('id')
+    
+    loading('正在上传')
+    submitProve(driverId,idCardR,idCardeB,campusCard,stuIdCard).then(res=>{
+      hideLoading()
+      wx.redirectTo({
+        url: '',
+      })
+    })
+  }
+})
---
+++ service/home.js
@@ -0,0 +1,68 @@
+import request from './network'
+import {
+  API_URL_getOrders,
+  API_URL_getOrdersDetail,
+  API_URL_updateOrderStatus 
+} from './config'
+
+// 抢单
+export function getOrders(id,orderId,orderNumber,riderId,shopId,status,userId){
+  return request({
+    url:API_URL_getOrders,
+    method:'POST',
+    data:{
+      id,
+      orderId,
+      orderNumber,
+      riderId,
+      shopId,
+      status,
+      userId
+    }
+  })
+}
+
+// 订单详情
+export function getOrdersDetail(pageNum,size,status,campus,riderId){
+  return request({
+    url: API_URL_getOrdersDetail,
+    method:'POST',
+    data:{
+      pageNum,
+      size,
+      status,
+      campus,
+      riderId
+    }
+    // data：data直接传个对象，调用时再具体写对应的参数（不怕参数对应出错，方便查看）
+  })
+}
+
+export function updateOrderStatus(id,orderId,orderNumber,shopId,status,userId){
+  return request({
+    url: API_URL_updateOrderStatus,
+    method:'POST',
+    data:{
+      id,
+      orderId,
+      orderNumber,
+      shopId,
+      status,
+      userId
+    }
+  })
+}
+
+export function getOrdersHistory(date,pageNumber,pageSize,riderId,shopId){
+  return request({
+    url: API_URL_getOrdersHistory,
+    method:'POST',
+    data:{
+      date,
+      pageNumber,
+      pageSize,
+      riderId,
+      shopId
+    }
+  })
+}
